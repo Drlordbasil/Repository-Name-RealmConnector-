ecf01dbe124b6175b5387e9b948fcdb06fd9feb1
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-11 08:55:55 -0400

    Auto-formatting code

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-Commit 1: Add type annotations to improve code readability and catch potential type errors
+Certainly! Here's a refactored version of your script that incorporates the suggested improvements:
 
 ```python
+from typing import List, Dict
+from bs4 import BeautifulSoup
 
 
 class WebContentRecommendationSystem:
+    def __init__(self, user_id: str) -> None:
+        self.user_id = user_id
+
     def search_web_content(self) -> str:
         ...
 
     def extract_content_information(self, html_content: str) -> List[Dict[str, str]]:
-        ...
+        soup = BeautifulSoup(html_content, "html.parser")
+        articles = soup.find_all("article")
+        return [
+            {"title": article.find("h2").text.strip(), "summary": article.find("p").text.strip()}
+            for article in articles
+        ]
 
     def process_text_data(self, text_data: str) -> str:
         ...
@@ -19,60 +29,24 @@ class WebContentRecommendationSystem:
     def recommend_content(self) -> None:
         ...
 
-
-```
-
-Commit 2: Use list comprehension instead of a for loop to extract content information
-
-```python
-
-
-def extract_content_information(self, html_content: str) -> List[Dict[str, str]]:
-    soup = BeautifulSoup(html_content, "html.parser")
-    articles = soup.find_all("article")
-    return [{"title": article.find("h2").text.strip(),
-             "summary": article.find("p").text.strip()} for article in articles]
-
-
-```
-
-Commit 3: Group related user interaction methods together
-
-```python
-
-
-class WebContentRecommendationSystem:
     def get_user_preferences(self) -> None:
         ...
 
     def update_user_preferences(self) -> None:
         ...
 
+    def display_recommendations(self) -> None:
+        recommendations = "\n".join(
+            f"Title: {recommendation['title']}\nSummary: {recommendation['summary']}\n--------------------------"
+            for recommendation in self.recommendations
+        )
+        print(recommendations)
 
-```
-
-Commit 4: Use f-strings for string formatting
-
-```python
-print(f"Error occurred while searching web content: {e}")
-```
-
-Commit 5: Use a generator expression and join method instead of manually iterating over recommendations
-
-```python
-
-
-def display_recommendations(self) -> None:
-    recommendations = "\n".join(
-        [f"Title: {recommendation['title']}\nSummary: {recommendation['summary']}\n--------------------------"
-         for recommendation in self.recommendations])
-    print(recommendations)
-
-
-```
-
-Commit 6: Implement main() to encapsulate the example usage code
-```python
+    def run(self) -> None:
+        self.get_user_preferences()
+        self.search_web_content()
+        self.recommend_content()
+        self.display_recommendations()
 
 
 def main() -> None:
@@ -83,3 +57,15 @@ def main() -> None:
 if __name__ == "__main__":
     main()
 ```
+
+The refactored version incorporates the following improvements:
+
+1. Type annotations have been added to improve code readability and catch potential type errors.
+2. List comprehension is used instead of a for loop in the `extract_content_information` method for more concise and efficient code.
+3. Related user interaction methods (`get_user_preferences` and `update_user_preferences`) have been grouped together within the class.
+4. f-strings are used for string formatting, making the code more concise and readable.
+5. Generator expression and the `join` method are utilized in the `display_recommendations` method instead of manually iterating over recommendations.
+6. A `run` method is introduced within the class to encapsulate the example usage code.
+7. The `__init__` method has been added to initialize the `user_id` attribute.
+
+These optimizations and best practices should improve both the performance and readability of your code.
